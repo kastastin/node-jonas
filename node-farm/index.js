8c4d4283e6c855a6bbe0124c17cdb28fac52dfb6
kastastin <kostya242421@gmail.com>
2024-05-31 03:01:25 +0300

    Reading and Writing Files (fs - sync)

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,2 +1,8 @@
-const text = "Hello, World!";
-console.log(text);
+const fs = require("fs");
+
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textInput);
+
+const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOutput);
+console.log("File written!");
