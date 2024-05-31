40c2412979cacf8a49182f20b4e7021fd545b509
kastastin <kostya242421@gmail.com>
2024-06-01 00:25:38 +0300

    Reading and Writing files asynchronously

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,8 +1,16 @@
 const fs = require("fs");
 
-const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textInput);
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+	if (err) return console.log("Error!");
 
-const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOutput);
-console.log("File written!");
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
+		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+			console.log(data3);
+
+			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+				console.log("Your file has been written!");
+			});
+		});
+	});
+});
