cefd54c180be425520352ca99ce8ea223284576e
kastastin <kostya242421@gmail.com>
2024-06-03 23:02:13 +0300

    Building a simple API

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
@@ -8,6 +12,9 @@ const server = http.createServer((req, res) => {
 		res.end("This is the OVERVIEW");
 	} else if (pathName === "/product") {
 		res.end("This is the PRODUCT");
+	} else if (pathName === "/api") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(data);
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
